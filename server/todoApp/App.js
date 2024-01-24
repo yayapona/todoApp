eeb4a66761092ebf5568417b95724e477cec5339
py <yayapona4@gmail.com>
2024-01-24 23:48:15 +0100

    server

--- server/todoApp/App.js
+++ server/todoApp/App.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const userRoute = require('./routes/userRoute');
 const taskRoute = require('./routes/taskRoute');
 const app = express();
+const AppError = require('./utils/appError');
+const handlerErrorHandler = require('./controllers/errorController')
 
 app.use(cors())
 app.use(express.json({ limit: '10kb' }));
@@ -11,6 +13,11 @@ app.use(express.json({ limit: '10kb' }));
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/tasks', taskRoute)
 
+app.all('*', (req,res,next) => {
+    next(new AppError(`Pas de route associé à cet url ${req.originalUrl}`));
+} )
+
+app.use(handlerErrorHandler)
 
 module.exports = app
 
